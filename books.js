e06339c5a9c8a72538b5cfac1e5229080b2f3bb1
ViktorChs <95919863+ViktorChs@users.noreply.github.com>
2023-02-28 09:56:50 -0400

    0.7

--- books.js
+++ books.js
@@ -1,6 +1,9 @@
 const print1 = document.querySelector('.content_books');
+const buttonSearch = document.querySelector('.btn-search');
+const valueSearch = document.querySelector('#search');
+const category = document.querySelector('select')
 
-bookArchive.forEach((book) =>{
+/*bookArchive.forEach((book) =>{
     const div =document.createElement('div')
     div.classList.add('book')
     div.innerHTML = `
@@ -8,4 +11,27 @@ bookArchive.forEach((book) =>{
     <h3>${book.nombre}</h3>
     `
     print1.appendChild(div)
+})*/
+
+
+buttonSearch.addEventListener('click', () =>{
+    deleteBooks(),search()
 })
+document.addEventListener('keyup', e =>{
+    console.log(e.target.value)
+})
+
+
+category.addEventListener('click',() =>{
+    filterM()
+})
+function filterM(){
+    console.log(category.value)
+}
+function search(){
+    console.log(valueSearch.value)
+}
+
+function deleteBooks(){
+    document.querySelector('.content_books').innerHTML = '';
+}
