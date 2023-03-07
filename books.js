6e6d0108faf58a096d517aa1f2c7c91b9781d249
ViktorChs <95919863+ViktorChs@users.noreply.github.com>
2023-03-06 23:00:45 -0400

    0.9 vercion

--- books.js
+++ books.js
@@ -3,6 +3,7 @@ const buttonSearch = document.querySelector(".btn-search");
 const valueSearch = document.querySelector("#search");
 const category = document.querySelector("select");
 
+
 bookArchive.forEach((book) => {
   const div = document.createElement("div");
   div.classList.add("book");
@@ -18,7 +19,7 @@ function a() {
     const div = document.createElement("div");
     div.classList.add("book");
     div.innerHTML = `
-        <img src=${book.img} alt="">
+        <img class="bookImg" src=${book.img} alt="">
         <h3>${book.nombre}</h3>
         `;
     print1.appendChild(div);
@@ -32,13 +33,13 @@ buttonSearch.addEventListener("click", () => {
 
 function filterM() {
     document.querySelector(".content_books").innerHTML = "";
-  
+    
      bookArchive.forEach((book) => {
       if (book.materia == category.value) {
         const div = document.createElement("div");
         div.classList.add("book");
         div.innerHTML = `
-      <img src=${book.img} alt="">
+      <img  src=${book.img} alt="">
       <h3>${book.nombre}</h3>
       `;
         print1.appendChild(div);
@@ -59,3 +60,22 @@ function filterM() {
 function deleteBooks() {
   document.querySelector(".content_books").innerHTML = "";
 }
+
+const bookmorf = document.querySelectorAll(".book")
+const bookmorfImg = document.querySelectorAll(".book img")
+
+
+
+function removeActiveMorfBooks() {
+  bookmorf.forEach(x => x.classList.add('active'));
+  bookmorfImg.forEach(x => x.classList.add('active'));
+  
+}
+
+function addActiveMorfBooks(){
+  bookmorf.forEach(x => x.classList.remove('active'));
+  bookmorfImg.forEach(x => x.classList.remove('active'));
+
+
+}
+
