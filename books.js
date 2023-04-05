6bf3b12558a0a667df998c3dab6838ad3d0a2a34
ViktorChs <95919863+ViktorChs@users.noreply.github.com>
2023-04-05 00:53:34 -0400

    V 0.1 new

--- books.js
+++ books.js
@@ -4,50 +4,238 @@ const valueSearch = document.querySelector("#search");
 const category = document.querySelector("select");
 const buttonsview = document.getElementById("#viewbookall")
 const viewmenubook = document.querySelector(".nav_menu-view-book")
+const newbooks = document.querySelector(".contenedor_books")
 
-/* entrada principal de los libros */
 bookArchive.forEach((book) => {
-  const div = document.createElement("div");
-  div.id.add
-  div.classList.add("book");
-  div.innerHTML = `
-    <img src=${book.img} alt="">
-    <h3>${book.nombre}</h3>
-    <button onclick="viewBook(${book.id})" id="viewbookall" class="viewBook" type="button"></button>
+  if (book.id <= 12) {
+    const div = document.createElement("div");
+    div.classList.add("newbooks");
+    div.setAttribute("onclick", `viewBooknew(${book.id})`);
+    div.innerHTML = `
+    <div class="imgcontent" ><img  src=${book.img} alt=""></div>
+    <div class="titlecontent" ><h3>${book.nombre}</h3></div>
+    </div>
+    
     `;
-  print1.appendChild(div);
-});
+    newbooks.appendChild(div);
+  }});
 
-/* filtro de materias */
-function filterM() {
-  document.querySelector(".content_books").innerHTML = "";
 
-  bookArchive.forEach((book) => {
-    if (book.materia == category.value) {
-      const div = document.createElement("div");
-      div.classList.add("book");
-      div.innerHTML = `
-      <img  src=${book.img} alt="">
-      <h3>${book.nombre}</h3>
-      <button onclick="viewBook(${book.id})" id="viewbookall" class="viewBook" type="button"></button>
-      `;
-      print1.appendChild(div);
-    } else if (category.value == "Materias" || book.id == Number) {
-      const div = document.createElement("div");
-      div.classList.add("book");
-      div.innerHTML = `
-      <img src=${book.img} alt="">
-      <h3>${book.nombre}</h3>
-      <button onclick="viewBook(${book.id})" id="viewbookall" class="viewBook" type="button"></button>
-
-      `;
-      print1.appendChild(div);
+  const cantidadLibros = bookArchive.reduce((contador, objeto) => {
+    if (objeto.formato === "libro") {
+      contador++;
     }
+    return contador;
+  }, 0);
+  
+  const cantidadRevistas = bookArchive.reduce((contador, objeto) => {
+    if (objeto.formato === "revista") {
+      contador++;
+    }
+    return contador;
+  }, 0);
+  const contenedornum = document.querySelector(".cantidadlibros")
+const numlr = document.createElement("div");
+numlr.classList.add("numberbook");
+numlr.innerHTML= `
+<div class="Txtlibro">
+<img src="icons8-open-book.gif" alt="">
+<span>${cantidadLibros}</span>
+<p> Total de libros </p>
+</div>
+
+<div class="centerimg"><img class="Logo" src="Logo.png" alt=""/></div>
+<div class="TxtRevista">
+<img src="icons8-magazine-50.png" alt="">
+<span>${cantidadRevistas}</span>
+<p>Total de revistas </p></div>
+`
+contenedornum.appendChild(numlr)
+/* entrada principal de los libros */
+
+// Obtener los checkboxes de los filtros
+let checkboxesFormato = document.querySelectorAll("#checklist-items-formato input[type=checkbox]");
+let checkboxesPNF = document.querySelectorAll("#checklist-items-PNF input[type=checkbox]");
+let checkboxesMateria = document.querySelectorAll("#checklist-items-materia input[type=checkbox]");
+
+// Función para obtener el valor de los checkboxes marcados
+function obtenerValoresCheckboxes() {
+
+  let valoresCheckboxFormato = Array.from(checkboxesFormato).filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
+  let valoresCheckboxPNF = Array.from(checkboxesPNF).filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
+  let valoresCheckboxMateria = Array.from(checkboxesMateria).filter((checkbox) => checkbox.checked).map((checkbox) => checkbox.value);
+
+  return {
+    formato: valoresCheckboxFormato,
+    pnf: valoresCheckboxPNF,
+    materia: valoresCheckboxMateria,
+  };
+}
+
+// Función para filtrar los libros según los valores de los checkboxes marcados
+
+function filtrarLibros() {
+
+  const valoresCheckboxes = obtenerValoresCheckboxes();
+
+  const startIndex = (currentPage - 1) * booksPerPage; // índice del primer libro en la página actual
+  const endIndex = startIndex + booksPerPage; // índice del último libro en la página actual
+  let librosFiltrados = bookArchive.slice(startIndex, endIndex); // libros a renderizar
+
+  if (valoresCheckboxes.formato.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.formato.includes(libro.formato));
+  }
+
+  if (valoresCheckboxes.pnf.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.pnf.includes(libro.pnf));
+  }
+
+  if (valoresCheckboxes.materia.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.materia.includes(libro.materia));
+  }
+  return librosFiltrados;
+}
+
+// Función para renderizar los libros filtrados
+function renderBooks() {
+  const librosFiltrados = filtrarLibros();
+
+  print1.innerHTML = ""; // limpiar el contenedor de libros
+  librosFiltrados.forEach((book) => {
+    const div = document.createElement("div");
+    div.setAttribute("onclick", `viewBook(${book.id})`);
+    div.classList.add("newbooks");
+    div.innerHTML = `
+      <div class="imgcontent"><img src="${book.img}" alt=""></div>
+      <div class="titlecontent"><h3>${book.nombre}</h3></div>
+    `;
+    print1.appendChild(div);
+    renderPagination()
+
   });
 }
 
-/* add event listeners */
+// Función para renderizar la barra de paginación
+// Función para filtrar los libros según los valores de los checkboxes marcados
+let currentPage = 1;
+const booksPerPage = 14;
+const totalPages = Math.ceil(bookArchive.length / booksPerPage);
 
+
+function filtrarLibros(currentPage) {
+  const valoresCheckboxes = obtenerValoresCheckboxes();
+
+  const startIndex = (currentPage - 1) * booksPerPage; // índice del primer libro en la página actual
+  const endIndex = startIndex + booksPerPage; // índice del último libro en la página actual
+  let librosFiltrados = bookArchive.slice(startIndex, endIndex); // libros a renderizar
+
+  if (valoresCheckboxes.formato.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.formato.includes(libro.formato));
+  }
+
+  if (valoresCheckboxes.pnf.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.pnf.includes(libro.pnf));
+  }
+
+  if (valoresCheckboxes.materia.length > 0) {
+    librosFiltrados = librosFiltrados.filter((libro) => valoresCheckboxes.materia.includes(libro.materia));
+  }
+
+  return librosFiltrados;
+}
+
+// Función para renderizar los libros filtrados
+function renderBooks(currentPage) {
+  const librosFiltrados = filtrarLibros(currentPage);
+
+  print1.innerHTML = ""; // limpiar el contenedor de libros
+  librosFiltrados.forEach((book) => {
+    const div = document.createElement("div");
+    div.setAttribute("onclick", `viewBook(${book.id})`);
+    div.classList.add("newbooks");
+    div.innerHTML = `
+      <div class="imgcontent"><img src="${book.img}" alt=""></div>
+      <div class="titlecontent"><h3>${book.nombre}</h3></div>
+    `;
+    print1.appendChild(div);
+  });
+}
+
+// Función para renderizar la barra de paginación
+function renderPagination() {
+  pagination.innerHTML = ""; // limpiar la barra de paginación
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.innerText = i;
+    button.classList.add("pagination-button");
+    if (i === currentPage) {
+      button.classList.add("active");
+    }
+    button.addEventListener("click", () => {
+      currentPage = i;
+      renderBooks(currentPage);
+      renderPagination();
+    });
+    pagination.appendChild(button);
+  }
+}
+
+// Agregar eventos a los checkboxes
+checkboxesFormato.forEach((checkbox) => checkbox.addEventListener("click", () => renderBooks(currentPage)));
+checkboxesPNF.forEach((checkbox) => checkbox.addEventListener("click", () => renderBooks(currentPage)));
+checkboxesMateria.forEach((checkbox) => checkbox.addEventListener("click", () => renderBooks(currentPage)));
+
+renderBooks(currentPage); // renderizar los libros en la página actual
+renderPagination(); // renderizar la barra de paginación
+
+/* add event listeners */
+function renderPagination() {
+  pagination.innerHTML = ""; // limpiar la barra de paginación
+  
+  // Agregar botón de flecha izquierda
+  const prevButton = document.createElement("button");
+  prevButton.innerHTML = '<i class="fas fa-chevron-left"><</i>';
+  prevButton.classList.add("pagination-button");
+  if (currentPage === 1) {
+    prevButton.disabled = true;
+  }
+  prevButton.addEventListener("click", () => {
+    currentPage--;
+    renderBooks(currentPage);
+    renderPagination();
+  });
+  pagination.appendChild(prevButton);
+
+  for (let i = 1; i <= totalPages; i++) {
+    const button = document.createElement("button");
+    button.innerText = i;
+    button.classList.add("pagination-button");
+    if (i === currentPage) {
+      button.classList.add("active");
+    }
+    button.addEventListener("click", () => {
+      currentPage = i;
+      renderBooks(currentPage);
+      renderPagination();
+    });
+    pagination.appendChild(button);
+  }
+
+  // Agregar botón de flecha derecha
+  const nextButton = document.createElement("button");
+  nextButton.innerHTML = '<i class="fas fa-chevron-right">></i>';
+  nextButton.classList.add("pagination-button");
+  if (currentPage === totalPages) {
+    nextButton.disabled = true;
+  }
+  nextButton.addEventListener("click", () => {
+    currentPage++;
+    renderBooks(currentPage);
+    renderPagination();
+  });
+  pagination.appendChild(nextButton);
+}
+/*
 buttonSearch.addEventListener("click", () => {
   console.log(valueSearch.value.toLowerCase());
 });
@@ -70,9 +258,35 @@ function addActiveMorfBooks() {
   bookbts.forEach((x) => x.classList.remove("active"));
 }
 /*   */
+function viewBooknew(prodId){
+  openbiblioteca()
+  viewmenubook.style.display = "flex";
 
- 
-
+  bookArchive.forEach((book) => {
+    if (prodId == book.id) {
+const div = document.createElement("div");
+  div.classList.add("nav_menu-background-active");
+  div.innerHTML = `
+  <div class="nav_menu-content-info-book">
+  <button class="nav_menu-bts-close-menu" >x</button>
+   <h2 class="nav_menu-h2-title-book">${book.nombre}</h2>
+   <img class="nav_menu-img-book" src="${book.img}" alt="">
+   <a class="nav_menu-a-description">${book.desc}description</a>
+   <a href="${book.link}" target="_blank" class="nav_menu-bts-download">Descarga<img src="download-rounded.svg" alt=""></a>
+   <img class="deco1" src="deco1.png" alt="a">
+   <img class="deco2" src="deco3.png" alt="b">
+   </div>
+    `;
+    viewmenubook.appendChild(div);
+    }})
+  
+  
+  const prod = bookArchive.map((prod) => {
+    if (prod.id == prodId) {
+      
+    }
+  })}
+/* */
 function viewBook(prodId){
   viewmenubook.style.display = "flex";
 
@@ -115,4 +329,19 @@ btsCloseMenu.addEventListener('click', () =>{
  
 })
 
+}
+const navbar = document.querySelector('.nav_bar')
+const openbiblio = document.querySelector('.vertodo')
+function openbiblioteca() {
+   toggleMenuFilters.style.display = "flex";
+  twoSection.style.display = "none";
+  threSection.style.display = "flex";
+  navbar.style.position = "unset";
+}
+openbiblio.addEventListener('click',() =>  {
+  openbiblioteca()
+})
+
+function aaa(){
+  document.querySelectorAll(".pagination-button")[1].click();
 }
--- js.js
+++ js.js
@@ -1,14 +1,179 @@
 const toggleSections = document.querySelector('.body_Content-Arrow')
 const oneSection = document.querySelector('.section_ini')
 const twoSection = document.querySelector('.section_2')
+const threSection = document.querySelector('.section_3')
+
+const toggleMenuFilters = document.querySelector('.etc')
+
+var checklistFormato = document.getElementById("checklist-Formato");
+var checklistPNF = document.getElementById("checklist-PNF");
+var checklistMateria = document.getElementById("checklist-materia");
+
+const toggleMenuf = () => document.body.classList.toggle("open");
 
 function timeouttoggle(){
     oneSection.style.display = "none";
     twoSection.style.display = "flex";
+    navbar.style.position = "absolute";
+    
+
+
+    document.body.style.background = "linear-gradient(144deg, #308fa3 0%, #ddc3b4 100%)"
 }
 
 toggleSections.addEventListener('click', () => {
     oneSection.style.animation = 'exittoggle 1s ease 0s 1 normal forwards';
     window.setTimeout(timeouttoggle, 1000);
+/* */
+    let checkboxes = document.querySelectorAll("#checklist-items-formato input[type=checkbox]");
+
+    function obtenerValorCheckboxMarcado() {
+      let valorCheckboxMarcado1 = "";
+      aaa()
+
+      checkboxes.forEach(function(checkbox) {
+        if (checkbox.checked) {
+          valorCheckboxMarcado1 = checkbox.value;
+        }
+      });
+    
+      console.log( valorCheckboxMarcado1);
+    }
+    
+    checkboxes.forEach(function(checkbox) {
+      checkbox.addEventListener("click", obtenerValorCheckboxMarcado);
+    });
+
+    let checkboxes12 = document.querySelectorAll("#checklist-items-PNF input[type=checkbox]");
+
+function obtenerValorCheckboxMarcado1() {
+  let valorCheckboxMarcado2 = "";
+aaa()
+  checkboxes12.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      valorCheckboxMarcado2 = checkbox.value;
+    }
+  });
+
+  console.log( valorCheckboxMarcado2);
+}
+
+checkboxes12.forEach(function(checkbox) {
+  checkbox.addEventListener("click", obtenerValorCheckboxMarcado1);
+});
+
+
+
+let checkboxes2 = document.querySelectorAll("#checklist-items-materia input[type=checkbox]");
+
+function obtenerValorCheckboxMarcado2() {
+  aaa()
+  let valorCheckboxMarcado3 = "";
+
+  checkboxes2.forEach(function(checkbox) {
+    if (checkbox.checked) {
+      valorCheckboxMarcado3 = checkbox.value;
+    }
+  });
+
+  console.log( valorCheckboxMarcado3);
+}
+
+checkboxes2.forEach(function(checkbox) {
+  checkbox.addEventListener("click", obtenerValorCheckboxMarcado2);
+});
+
+
+/*-------------------------*/
+
+
+
+
+
 
 }  )
+checklistFormato.addEventListener("click", function() {
+  var checkboxes = document.querySelectorAll("#checklist-items-formato");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].style.display === "none") {
+      checkboxes[i].style.display = "block";
+    } else {
+      checkboxes[i].style.display = "none";
+    }
+  }
+});
+checklistPNF.addEventListener("click", function() {
+  var checkboxes = document.querySelectorAll("#checklist-items-PNF");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].style.display === "none") {
+      checkboxes[i].style.display = "block";
+    } else {
+      checkboxes[i].style.display = "none";
+    }
+  }
+});
+checklistMateria.addEventListener("click", function() {
+  var checkboxes = document.querySelectorAll("#checklist-items-materia");
+  for (var i = 0; i < checkboxes.length; i++) {
+    if (checkboxes[i].style.display === "none") {
+      checkboxes[i].style.display = "block";
+    } else {
+      checkboxes[i].style.display = "none";
+    }
+  }
+});
+
+
+let checkboxes = document.querySelectorAll("#checklist-items-PNF input[type=checkbox]");
+for (let i = 0; i < checkboxes.length; i++) {
+  checkboxes[i].addEventListener("click", function() {
+    if (this.checked) {
+      for (let j = 0; j < checkboxes.length; j++) {
+        if (checkboxes[j] !== this) {
+          checkboxes[j].parentNode.style.display = "none"; // Oculta el elemento li que contiene el checkbox y su label asociado
+        }
+      }
+    } else {
+      for (let j = 0; j < checkboxes.length; j++) {
+        checkboxes[j].parentNode.style.display = "flex"; // Muestra todos los elementos li que contienen los checkboxes y sus labels asociados
+      }
+    }
+  });
+}
+let checkboxes1 = document.querySelectorAll("#checklist-items-materia input[type=checkbox]");
+for (let i = 0; i < checkboxes1.length; i++) {
+  checkboxes1[i].addEventListener("click", function() {
+    if (this.checked) {
+      for (let j = 0; j < checkboxes1.length; j++) {
+        if (checkboxes1[j] !== this) {
+          
+          checkboxes1[j].parentNode.style.display = "none"; // Oculta el elemento li que contiene el checkbox y su label asociado
+        }
+      }
+    } else {
+      for (let j = 0; j < checkboxes1.length; j++) {
+        
+        checkboxes1[j].parentNode.style.display = "flex"; // Muestra todos los elementos li que contienen los checkboxes y sus labels asociados
+      }
+    }
+  });
+}
+let checkboxes2 = document.querySelectorAll("#checklist-items-formato input[type=checkbox]");
+for (let i = 0; i < checkboxes2.length; i++) {
+  checkboxes2[i].addEventListener("click", function() {
+    if (this.checked) {
+      for (let j = 0; j < checkboxes2.length; j++) {
+        if (checkboxes2[j] !== this) {
+          checkboxes2[j].parentNode.style.display = "none"; // Oculta el elemento li que contiene el checkbox y su label asociado
+        }
+      }
+    } else {
+      for (let j = 0; j < checkboxes2.length; j++) {
+        checkboxes2[j].parentNode.style.display = "flex"; // Muestra todos los elementos li que contienen los checkboxes y sus labels asociados
+      }
+    }
+  });
+}
+
+
+
